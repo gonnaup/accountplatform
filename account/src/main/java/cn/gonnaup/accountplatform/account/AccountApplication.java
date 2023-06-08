549a235e25970f59d22e87f6890c95a296a3fd7d
gonnaup <gonnaup@163.com>
2023-06-08 23:26:06 +0800

    帐号平台项目初始化

---
+++ account/src/main/java/cn/gonnaup/accountplatform/account/AccountApplication.java
@@ -0,0 +1,17 @@
+package cn.gonnaup.accountplatform.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 帐号平台启动类
+ *
+ * @author gonnaup
+ * @version created at 2023/5/31 下午7:00
+ */
+@SpringBootApplication
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
